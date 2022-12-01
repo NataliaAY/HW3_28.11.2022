6952e7af0738b47a662d3dd11f55f73ef0604b7d
Natalia <averegina@male.ru>
2022-12-01 21:54:52 +0300

    решена задача1_v.4

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Ура, это палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Увы, это не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"{number} - вообще не пятизначное");
+}
+
